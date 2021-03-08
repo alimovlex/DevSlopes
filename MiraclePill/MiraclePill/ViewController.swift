c464b0bc09ee834eb4b1df4b520568423bdc7467
alimovlex <alimovlex@hotmail.com>
2021-03-09 05:53:26 +0700

    The first lesson on autolayout is complete.

--- MiraclePill/MiraclePill/ViewController.swift
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -11,10 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view.
     }
-    var window: UIWindow?;
-
+    
 }
 
