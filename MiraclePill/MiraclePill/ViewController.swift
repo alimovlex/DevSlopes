8e5e48542486f3951a087785e66ed632fb201d70
alimovlex <alimovlex@hotmail.com>
2021-03-09 03:35:54 +0700

    The second demo app has been initialized.

---
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by robot on 3/9/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    var window: UIWindow?;
+
+}
+
