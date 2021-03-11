670b114bb331c0a91ed572fcff06414685e745e6
alimovlex <alimovlex@hotmail.com>
2021-03-12 03:43:06 +0700

    The custom sound has been added.

--- RetroCalculator/RetroCalculator/ViewController.swift
+++ RetroCalculator/RetroCalculator/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var btnSound: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav");
+        let soundURL = URL(fileURLWithPath: path!);
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: soundURL);
+            btnSound.prepareToPlay();
+        } catch let err as NSError {
+            print(err.debugDescription);
+        }
     }
-
+    
+    @IBAction func numberPressed(sender:UIButton) {
+        playSound();
+    }
+    
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop();
+        }
+        btnSound.play();
+    }
 
 }
 
