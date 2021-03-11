0712acddd7718fd16e7f7f2c2e9d18b8698e78b5
alimovlex <alimovlex@hotmail.com>
2021-03-11 22:46:53 +0700

    The lesson on segues has been performed.

---
+++ SwappingScreens/SwappingScreens/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by robot on 3/11/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ SwappingScreens/SwappingScreens/MusicListVC.swift
@@ -0,0 +1,36 @@
+//
+//  MusicListVC.swift
+//  SwappingScreens
+//
+//  Created by robot on 3/11/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        view.backgroundColor = UIColor.blue;
+    }
+    
+
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil);
+    }
+    
+    @IBAction func load3rdScreenPressed(_ sender: Any) {
+        let songTitle = "Quit Playing Games With My Heart";
+        performSegue(withIdentifier: "PlaySongVC", sender: songTitle);
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? PlaySongVC {
+            if let song = sender as? String {
+            destination.selectedSong = song;
+        }
+        }
+    }
+    
+}
