6d58bb026f15ded2f2c276530fcf50e6ba7a7a25
alimovlex <alimovlex@hotmail.com>
2021-03-12 11:41:03 +0700

    The party rock application has been completed.

--- PartyRockApp/PartyRockApp/Controller/MainVC.swift
+++ PartyRockApp/PartyRockApp/Controller/MainVC.swift
@@ -17,12 +17,12 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad();
         
-        let p1 = PartyRock(imageURL: "https://i.ytimg.com/vi/SjmTFIHX1yo/maxresdefault.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Lights Out");
+        let p1 = PartyRock(imageURL: "https://i.ytimg.com/vi/SjmTFIHX1yo/maxresdefault.jpg", videoURL: "<iframe width=\"320\" height=\"175\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Lights Out");
         
-        let p2 = PartyRock(imageURL: "https://i.ytimg.com/vi/KvgpWoYgHH4/maxresdefault.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/1w9DiGlZksU\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Let's Get Ridiculous");
-        let p3 = PartyRock(imageURL: "https://i.ytimg.com/vi/2wUxw6GH3IM/hqdefault.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/vg_nvEGryA4\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Juicy Wiggle Lesson");
-        let p4 = PartyRock(imageURL: "https://i.ytimg.com/vi/5oi4RsL2kwk/maxresdefault.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/gZIqW92GaTQ\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Party Rock Commercial");
-        let p5 = PartyRock(imageURL: "https://i.ytimg.com/vi/k7OtcWVaT5s/maxresdefault.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/tWyuglGCKgE\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Juicy Wiggle");
+        let p2 = PartyRock(imageURL: "https://i.ytimg.com/vi/KvgpWoYgHH4/maxresdefault.jpg", videoURL: "<iframe width=\"320\" height=\"175\" src=\"https://www.youtube.com/embed/1w9DiGlZksU\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Let's Get Ridiculous");
+        let p3 = PartyRock(imageURL: "https://i.ytimg.com/vi/2wUxw6GH3IM/hqdefault.jpg", videoURL: "<iframe width=\"320\" height=\"175\" src=\"https://www.youtube.com/embed/vg_nvEGryA4\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Juicy Wiggle Lesson");
+        let p4 = PartyRock(imageURL: "https://i.ytimg.com/vi/5oi4RsL2kwk/maxresdefault.jpg", videoURL: "<iframe width=\"320\" height=\"175\" src=\"https://www.youtube.com/embed/gZIqW92GaTQ\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Party Rock Commercial");
+        let p5 = PartyRock(imageURL: "https://i.ytimg.com/vi/k7OtcWVaT5s/maxresdefault.jpg", videoURL: "<iframe width=\"320\" height=\"175\" src=\"https://www.youtube.com/embed/tWyuglGCKgE\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Juicy Wiggle");
         
         partyRocks.append(p1)
         partyRocks.append(p2)
@@ -51,6 +51,21 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return partyRocks.count;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let partyRock = partyRocks[indexPath.row];
+        
+        performSegue(withIdentifier: "VideoVC", sender: partyRock);
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? VideoVC {
+            
+            if let party = sender as? PartyRock {
+                destination.partyRock = party;
+            }
+        }
     }
 
 }
