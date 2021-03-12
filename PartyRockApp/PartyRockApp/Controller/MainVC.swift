f6e87ca912a39ffd718dd3d0a35585b72209fd41
alimovlex <alimovlex@hotmail.com>
2021-03-12 07:35:23 +0700

    The party rock application has been started.

---
+++ PartyRockApp/PartyRockApp/Controller/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by robot on 3/12/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ PartyRockApp/PartyRockApp/Controller/VideoVC.swift
@@ -0,0 +1,30 @@
+//
+//  VideoVC.swift
+//  PartyRockApp
+//
+//  Created by robot on 3/12/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
