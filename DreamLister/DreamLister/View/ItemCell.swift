f6fa1c8f490f2c4c76db89577ce7a5a4631e754a
alimovlex <alimovlex@hotmail.com>
2021-03-14 09:07:57 +0700

    The tableView has been added and configured.

---
+++ DreamLister/DreamLister/View/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by robot on 3/14/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
