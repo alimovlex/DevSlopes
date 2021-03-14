d112080878bf2eba3efd1dbbfd5f51ee74ed4b39
alimovlex <alimovlex@hotmail.com>
2021-03-14 09:08:55 +0700

    The tableView undergo configuration.

--- DreamLister/DreamLister/View/ItemCell.swift
+++ DreamLister/DreamLister/View/ItemCell.swift
@@ -10,15 +10,9 @@ import UIKit
 
 class ItemCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var thumb: UIImageView!;
+    
+    @IBOutlet weak var details: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var title: UILabel!
 }
