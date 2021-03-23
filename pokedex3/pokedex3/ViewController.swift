f16da808012dcc1b07b351c88f76edd0599540cd
alimovlex <alimovlex@hotmail.com>
2021-03-23 21:23:35 +0700

    The pokemon app project has been initialized.

---
+++ pokedex3/pokedex3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by robot on 3/23/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
