a42db43b677923909de812cd423e51ecbda7b660
alimovlex <alimovlex@hotmail.com>
2021-03-26 06:33:15 +0700

    The search algorythm has been done. The keyboard dismissal on button and on tap has been added too.

--- pokedex3/pokedex3/Controller/ViewController.swift
+++ pokedex3/pokedex3/Controller/ViewController.swift
@@ -9,18 +9,27 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     @IBOutlet weak var collection: UICollectionView!;
+    @IBOutlet weak var searchBar: UISearchBar!;
     
     var pokemon = [Pokemon]();
+    var filteredPokemon = [Pokemon]();
     var musicPlayer: AVAudioPlayer!;
+    var inSearchMode = false;
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        //dismissing the keyboard on tap
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing));
+        view.addGestureRecognizer(tap);
         //let charmander = Pokemon(name: "Charmander", pokedexId: 4);
         collection.dataSource = self;
         collection.delegate = self;
+        searchBar.delegate = self;
+        
+        searchBar.returnKeyType = UIReturnKeyType.done;
         
         parsePokemonCSV();
         initAudio();
@@ -65,7 +74,18 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
             
-            let poke = pokemon[indexPath.row];
+            let poke: Pokemon!;
+            
+            if inSearchMode {
+                
+                poke = filteredPokemon[indexPath.row];
+                cell.configureCell(pokemon: poke);
+            } else {
+                
+                poke = pokemon[indexPath.row];
+                cell.configureCell(pokemon: poke);
+            }
+            
             cell.configureCell(pokemon: poke);
             
             return cell;
@@ -79,7 +99,11 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    
+        
+        if inSearchMode {
+            return filteredPokemon.count;
+        }
+        
         return pokemon.count;
     }
     
@@ -104,6 +128,29 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             sender.alpha = 1.0;
         }
     }
-
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false;
+            collection.reloadData();
+            //view.endEditing(true); //dismiss the keyboard on deletion
+        } else {
+            inSearchMode = true;
+            
+            let lower = searchBar.text!.lowercased();
+            filteredPokemon = pokemon.filter({$0.name.range(of: lower) != nil});
+            collection.reloadData();
+            
+        }
+    }
+    //dismissing the keyboard on search button pressed
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+    {
+        inSearchMode = false;
+        self.searchBar.endEditing(true);
+        //view.endEditing(true); forcing the view to dismiss the keyboard
+    }
+    
 }
 
