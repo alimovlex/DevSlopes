bbf34f978ac20331a060d2b2c89e00370e67cb95
alimovlex <alimovlex@hotmail.com>
2021-03-27 05:11:09 +0700

    The openweatherrequest works properly now.

--- rainyshinycloudy/rainyshinycloudy/CurrentWeather.swift
+++ rainyshinycloudy/rainyshinycloudy/CurrentWeather.swift
@@ -2,80 +2,111 @@
 //  CurrentWeather.swift
 //  rainyshinycloudy
 //
-//  Created by robot on 3/22/21.
+//  Created by robot on 7/27/16.
 //  Copyright © 2021 robot. All rights reserved.
-// THE MODEL OF THE APPLICATION
+//
 
 import UIKit
 import Alamofire
 
 class CurrentWeather {
-    var _cityName: String!;
-    var _date: String!;
-    var _weatherType: String!;
-    var _currentTemp: Double!;
-    //var _highTemp: String!;
+    var _cityName: String!
+    var _date: String!
+    var _weatherType: String!
+    var _currentTemp: Double!
+    
     var cityName: String {
         if _cityName == nil {
-            _cityName = "";
+            _cityName = ""
         }
-        return _cityName;
+        return _cityName
     }
     
     var date: String {
         if _date == nil {
-            _date = "";
+            _date = ""
         }
         
-        let dateFormatter = DateFormatter();
-        dateFormatter.dateStyle = .long;
-        dateFormatter.timeStyle = .none;
-        let currentDate = dateFormatter.string(from: Date());
-        self._date = "Today, \(currentDate)";
-        return _date;
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        let currentDate = dateFormatter.string(from: Date())
+        self._date = "Today, \(currentDate)"
+        return _date
     }
     
     var weatherType: String {
         if _weatherType == nil {
-            _weatherType = "";
+            _weatherType = ""
         }
-        return _weatherType;
+        return _weatherType
     }
     
     var currentTemp: Double {
         if _currentTemp == nil {
-            _currentTemp = 0.0;
+            _currentTemp = 0.0
         }
-        return _currentTemp;
+        return _currentTemp
     }
     
-    func downloadWeatherDetails(completed: DownloadComplete) {
-        //Alamofire download
-        let currentWeatherURL = URL(string: CURRENT_WEATHER_URL)!;
-        Alamofire.request(currentWeatherURL).responseJSON { response  in //response == nil???
-            let result = response.result;
+    func downloadWeatherDetails(completed: @escaping DownloadComplete) {
+        //Download Current Weather Data
+        Alamofire.request(CURRENT_WEATHER_URL).responseJSON { response in
+            let result = response.result
+            
             if let dict = result.value as? Dictionary<String, AnyObject> {
-                    if let name = dict["name"] as? String {
-                    self._cityName = name.capitalized;
-                        print(name);
+                
+                if let name = dict["name"] as? String {
+                    self._cityName = name.capitalized
+                    if let namecity = self._cityName {
+                        print(namecity);
+                    } else {
+                        print("NULL");
                     }
-                    if let weather = dict["weather"] as? [Dictionary<String,AnyObject>] {
-                        if let main = weather[0]["main"] as? String {
-                            self._weatherType = main.capitalized;
-                            print(main);
+                }
+                
+                if let weather = dict["weather"] as? [Dictionary<String, AnyObject>] {
+                    
+                    if let main = weather[0]["main"] as? String {
+                        self._weatherType = main.capitalized
+                        if let typeweather = self._weatherType {
+                            print(typeweather);
+                        } else {
+                            print("NULL");
                         }
                     }
+                
+                }
+                
+                if let main = dict["main"] as? Dictionary<String, AnyObject> {
                     
-                    if let main = dict["main"] as? Dictionary<String, AnyObject> {
-                        if let currentTemperature = main["temp"] as? Double {
-                            let kelvinToCelsius = currentTemperature - 273.15;
-                            self._currentTemp = kelvinToCelsius;
-                            print(kelvinToCelsius);
+                    if let currentTemperature = main["temp"] as? Double {
+                    
+                        let kelvinToCelsius = currentTemperature - 273.2;
+                        
+                        self._currentTemp = kelvinToCelsius
+                        if let tempnow = self._currentTemp {
+                            print(tempnow);
+                        } else {
+                            print("NULL");
                         }
                     }
                 }
+            }
+            completed()
         }
-        completed();
     }
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
