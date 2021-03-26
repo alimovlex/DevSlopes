5d7c885c54a3666f58f85c32c12f7e4bc8e65dd6
alimovlex <alimovlex@hotmail.com>
2021-03-26 22:38:56 +0700

    All IBoutlets have been set.

--- pokedex3/pokedex3/Controller/PokemonDetailVC.swift
+++ pokedex3/pokedex3/Controller/PokemonDetailVC.swift
@@ -9,16 +9,37 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
-
+    
+    var pokemon: Pokemon!;
 
     @IBOutlet weak var nameLbl: UILabel!;
-    var pokemon: Pokemon!;
+    @IBOutlet weak var mainImg: UIImageView!;
+    
+    @IBOutlet weak var descriptionLbl: UILabel!;
+    @IBOutlet weak var typeLbl: UILabel!;
+    
+    @IBOutlet weak var defenseLbl: UILabel!;
+    
+    @IBOutlet weak var heightLbl: UILabel!;
+    @IBOutlet weak var pokedexLbl: UILabel!;
+    @IBOutlet weak var weightLbl: UILabel!;
+    @IBOutlet weak var attackLbl: UILabel!;
+    @IBOutlet weak var currentEvoimg: UIImageView!;
+    @IBOutlet weak var nextEvoimg: UIImageView!;
+    @IBOutlet weak var evoLbl: UILabel!;
+   
+    
     
     override func viewDidLoad() {
         super.viewDidLoad();
         
         nameLbl.text = pokemon.name;
         
+    }
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil);
     }
     
 }
