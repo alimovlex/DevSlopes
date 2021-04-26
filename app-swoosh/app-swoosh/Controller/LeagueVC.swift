111fe931148c60d2d219ba45ab359692a8113b52
alimovlex <alimovlex@hotmail.com>
2021-04-26 14:30:44 +0700

    The third screen has been binned with application.

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -16,6 +16,10 @@ class LeagueVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func onNextTapped(_ sender: Any) {
+        //Programmatic segue on button tap.
+        performSegue(withIdentifier: "skillVCSegue", sender: self);
+    }
 
     /*
     // MARK: - Navigation
