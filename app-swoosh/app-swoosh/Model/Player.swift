54b5ebcd1c5d26131d9014facff397a4c0e04b07
alimovlex <alimovlex@hotmail.com>
2021-04-26 17:16:43 +0700

    The IBActions have been implemented.

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by robot on 4/26/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?;
+    var selectedSkillLevel: String?;
+}
