7b15472cd86568590c36f9f4c3916c179a93ad7c
alimovlex <alimovlex@hotmail.com>
2021-04-27 16:07:03 +0700

    The first lesson on window-shopper application is complete!

---
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by robot on 4/27/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
