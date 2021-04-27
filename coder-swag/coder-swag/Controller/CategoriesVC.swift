535d74ef371993848b16e399b009311254e6608f
alimovlex <alimovlex@hotmail.com>
2021-04-28 03:23:31 +0700

    The first lesson on coder-swag application is complete!

---
+++ coder-swag/coder-swag/Controller/CategoriesVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by robot on 4/28/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    @IBOutlet weak var categoryTable: UITableView!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
