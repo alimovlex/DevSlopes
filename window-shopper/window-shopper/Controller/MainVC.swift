373c985232ee1924b6053b79e9c0ced389885a3c
alimovlex <alimovlex@hotmail.com>
2021-04-27 18:36:36 +0700

    The application logic is finally done. The currency formatter will be ahead!

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -12,11 +12,14 @@ class MainVC: UIViewController {
 
     
     @IBOutlet weak var wageTxt: CurrencyTxtField!;
-    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!;
+    @IBOutlet weak var resultLbl: UILabel!;
+    @IBOutlet weak var hoursLbl: UILabel!;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing));
+        view.addGestureRecognizer(tap);
         let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60));
         calcBtn.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1);
         calcBtn.setTitle("Calculate", for: .normal);
@@ -25,12 +28,29 @@ class MainVC: UIViewController {
         
         wageTxt.inputAccessoryView = calcBtn;
         priceTxt.inputAccessoryView = calcBtn;
+        
+        resultLbl.isHidden = true;
+        hoursLbl.isHidden = true;
     }
     
     @objc func calculate() {
-        print("We got here");
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text {
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                view.endEditing(true);
+                resultLbl.isHidden = false;
+                hoursLbl.isHidden = false;
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))";
+            }
+        }
     }
-
+    
+    @IBAction func clearCalculatorPressed(_ sender: Any) {
+        resultLbl.isHidden = true;
+        hoursLbl.isHidden = true;
+        wageTxt.text = "";
+        priceTxt.text = "";
+    }
+    
 
 }
 
