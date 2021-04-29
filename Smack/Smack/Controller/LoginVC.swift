bd4e125d662f48f09d8c1319f174ec01e0e57042
alimovlex <alimovlex@hotmail.com>
2021-04-30 05:04:32 +0700

    The loginVC has been implemented!

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by robot on 4/30/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil);
+    }
+    
+
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by robot on 4/30/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin";
