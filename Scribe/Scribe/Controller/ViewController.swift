e690e6f01686641acfda1cee15839169517fb0c0
alimovlex <alimovlex@hotmail.com>
2021-04-29 17:25:03 +0700

    The recording and playing is working properly!

--- Scribe/Scribe/Controller/ViewController.swift
+++ Scribe/Scribe/Controller/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
     @IBOutlet weak var activitySpinner: UIActivityIndicatorView!;
     @IBOutlet weak var transcriptionTextField: UITextView!;
     
+    @IBOutlet weak var recordButton: UIButton!;
+    @IBOutlet weak var playButton: UIButton!;
+    
     var audioRecorder: AVAudioRecorder!;
     var audioPlayer: AVAudioPlayer!;
     var recordingSession: AVAudioSession!;
@@ -23,13 +26,32 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
     override func viewDidLoad() {
         super.viewDidLoad()
         activitySpinner.isHidden = true;
+        self.setupView();
     }
+    
     
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
         player.stop();
+        recordButton.isEnabled = true
+        playButton.setTitle("Play", for: .normal)
         activitySpinner.stopAnimating();
         activitySpinner.isHidden = true;
     }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        print("Error while recording audio \(error!.localizedDescription)")
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        print("Error while playing audio \(error!.localizedDescription)")
+    }
+    
     
     //Reading the pre-recorded audio file
     func requestSpeechAuth() {
@@ -59,12 +81,127 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
         }
     }
     
+    func loadRecordingUI() {
+        recordButton.isEnabled = true
+        playButton.isEnabled = false
+        recordButton.setTitle("Tap to Record", for: .normal)
+        recordButton.addTarget(self, action: #selector(recordAudioButtonTapped), for: .touchUpInside)
+        view.addSubview(recordButton)
+    }
+    
+    func setupView() {
+           recordingSession = AVAudioSession.sharedInstance()
+           
+           do {
+               try recordingSession.setCategory(.playAndRecord, mode: .default)
+               try recordingSession.setActive(true)
+               recordingSession.requestRecordPermission() { [unowned self] allowed in
+                   DispatchQueue.main.async {
+                       if allowed {
+                           self.loadRecordingUI()
+                       } else {
+                           // failed to record
+                       }
+                   }
+               }
+           } catch {
+               // failed to record
+           }
+       }
+    
+    func startRecording() {
+        let audioFilename = getFileURL()
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatAppleLossless),
+            AVSampleRateKey: 320000,
+            AVNumberOfChannelsKey: 2,
+            AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue
+        ]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            
+            recordButton.setTitle("Tap to Stop", for: .normal)
+            playButton.isEnabled = false
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+            // recording failed :(
+        }
+        
+        playButton.isEnabled = true
+        recordButton.isEnabled = true
+    }
+    
+    func preparePlayer() {
+        var error: NSError?
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: getFileURL() as URL)
+        } catch let error1 as NSError {
+            error = error1
+            audioPlayer = nil
+        }
+        
+        if let err = error {
+            print("AVAudioPlayer error: \(err.localizedDescription)")
+        } else {
+            audioPlayer.delegate = self
+            audioPlayer.prepareToPlay()
+            audioPlayer.volume = 10.0
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func getFileURL() -> URL {
+        let path = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        return path as URL
+    }
+    
     
     @IBAction func playBtnPressed(_ sender: Any) {
         activitySpinner.isHidden = false;
         activitySpinner.startAnimating();
         requestSpeechAuth();
     }
+    
+    @IBAction func recordAudioButtonTapped(_ sender: UIButton) {
+        if audioRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    
+    @IBAction func playAudioButtonTapped(_ sender: UIButton) {
+        if (sender.titleLabel?.text == "Play"){
+                   recordButton.isEnabled = false
+                   sender.setTitle("Stop", for: .normal)
+                   preparePlayer()
+                   audioPlayer.play()
+               } else {
+                   audioPlayer.stop()
+                   sender.setTitle("Play", for: .normal)
+               }
+    }
+    
+    
     
 }
 
