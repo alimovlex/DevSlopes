a68541b0f3abc8afd738fa62d5227e6dee70f6dd
alimovlex <alimovlex@hotmail.com>
2021-04-30 18:16:34 +0700

    The workspace was organized due to the course sequential order.

---
+++ iOS 11/pixel-city/pixel-city/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by robot on 4/30/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
