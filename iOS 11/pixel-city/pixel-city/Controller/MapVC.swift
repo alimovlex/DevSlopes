a6a615fe72c23f8254219c1abca43d2eaeb3659c
alimovlex <alimovlex@hotmail.com>
2021-05-03 03:40:09 +0700

    The image array reload function has been added!

--- iOS 11/pixel-city/pixel-city/Controller/MapVC.swift
+++ iOS 11/pixel-city/pixel-city/Controller/MapVC.swift
@@ -44,7 +44,7 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "photoCell");
         collectionView?.delegate = self;
         collectionView?.dataSource = self;
-        collectionView?.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1);
+        collectionView?.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1);
         
         pullUpView.addSubview(collectionView!);
     }
@@ -150,6 +150,11 @@ extension MapVC: MKMapViewDelegate {
         removeProgressLbl();
         cancelAllSessions();
         
+        imageUrlArray = [];
+        imageArray = [];
+        
+        collectionView?.reloadData();
+        
         animateViewUp();
         addSwipe();
         addSpinner();
@@ -170,7 +175,7 @@ extension MapVC: MKMapViewDelegate {
                     if finished {
                         self.removeSpinner();
                         self.removeProgressLbl();
-                        // reload collectionView
+                        self.collectionView?.reloadData();
                     }
                 }
             }
@@ -184,7 +189,6 @@ extension MapVC: MKMapViewDelegate {
     }
     
     func retrieveUrls(forAnnotation annotation: DroppablePin, handler: @escaping (_ status: Bool) -> ()) {
-        imageUrlArray = [];
         
         Alamofire.request(flickrUrl(forApiKey: apiKey, withAnnotation: annotation, andNumberOfPhotos: 40)).responseJSON { (response) in
             guard let json = response.result.value as? Dictionary<String, AnyObject> else {return;}
@@ -199,7 +203,6 @@ extension MapVC: MKMapViewDelegate {
     }
     
     func retrieveImages(handler: @escaping (_ status: Bool) -> ()) {
-        imageArray = [];
         
         for url in imageUrlArray {
             Alamofire.request(url).responseImage(completionHandler: { (response) in
@@ -245,12 +248,15 @@ extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //number of items in arrau.
-        return 4;
+        //number of items in array.
+        return imageArray.count;
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell;
-        return cell!;
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell else {return UICollectionViewCell()}
+        let imageFromIndex = imageArray[indexPath.row];
+        let imageView = UIImageView(image: imageFromIndex);
+        cell.addSubview(imageView);
+        return cell;
     }
 }
