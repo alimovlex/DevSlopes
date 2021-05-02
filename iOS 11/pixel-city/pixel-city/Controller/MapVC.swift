86abf36cbfcee59c6241b4c9e084d94f7fd1b522
alimovlex <alimovlex@hotmail.com>
2021-05-02 21:54:45 +0700

    The pull up view has been devised. The spinner has been integrated.

--- iOS 11/pixel-city/pixel-city/Controller/MapVC.swift
+++ iOS 11/pixel-city/pixel-city/Controller/MapVC.swift
@@ -13,10 +13,17 @@ import CoreLocation
 class MapVC: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!;
+    @IBOutlet weak var pullUpView: UIView!;
+    @IBOutlet weak var pullUpViewHeightConstraint: NSLayoutConstraint!;
     
     var locationManager = CLLocationManager();
     let authorizationStatus = CLLocationManager.authorizationStatus();
     let regionRadius: Double = 500;
+    
+    var screenSize = UIScreen.main.bounds;
+    
+    var spinner: UIActivityIndicatorView?;
+    var progressLbl: UILabel?;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +45,35 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         doubleTap.numberOfTapsRequired = 2;
         doubleTap.delegate = self;
         mapView.addGestureRecognizer(doubleTap);
+    }
+    
+    func addSwipe() {
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animateViewDown));
+        swipe.direction = .down;
+        pullUpView.addGestureRecognizer(swipe);
+    }
+    
+    func animateViewUp() {
+        pullUpViewHeightConstraint.constant = 150;
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded();
+        }
+    }
+    
+   @objc func animateViewDown() {
+        pullUpViewHeightConstraint.constant = 0;
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded();
+        }
+    }
+    
+    func addSpinner() {
+        spinner = UIActivityIndicatorView();
+        spinner?.center = CGPoint(x: (screenSize.width/2) - ((spinner?.frame.width)!/2), y: 75);
+        spinner?.style = .whiteLarge;
+        spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1);
+        spinner?.startAnimating();
+        pullUpView.addSubview(spinner!);
     }
     
     func deviceLocation() ->String {
@@ -72,6 +108,9 @@ extension MapVC: MKMapViewDelegate {
     
     @objc func dropPin(sender: UITapGestureRecognizer) {
         removePin();
+        animateViewUp();
+        addSwipe();
+        addSpinner();
         let touchPoint = sender.location(in: mapView);
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView);
         
