9b79f1ccd980b8fa60bf0a5dbea0858d2b3cba3d
alimovlex <alimovlex@hotmail.com>
2021-05-02 10:27:55 +0700

    The GPS has been configured! The location is displaying properly!

--- iOS 11/pixel-city/pixel-city/Controller/MapVC.swift
+++ iOS 11/pixel-city/pixel-city/Controller/MapVC.swift
@@ -8,22 +8,61 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!;
     
+    var locationManager = CLLocationManager();
+    let authorizationStatus = CLLocationManager.authorizationStatus();
+    let regionRadius: Double = 500;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self;
+        locationManager.delegate = self;
+        configureLocationServices();
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.locationManager.distanceFilter = kCLDistanceFilterNone;
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        self.locationManager.startUpdatingLocation();
+        print(self.deviceLocation());
+    }
+    
+    func deviceLocation() ->String {
+        return "Longtitude: \(self.locationManager.location!.coordinate.longitude) Latitude: \(self.locationManager.location!.coordinate.latitude)"
     }
     
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
-        
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation();
+        }
     }
-}
-
-extension MapVC: MKMapViewDelegate {
     
 }
 
+extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else {return}
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius);
+        mapView.setRegion(coordinateRegion, animated: true);
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization();
+            locationManager.startUpdatingLocation();
+        } else {
+            return;
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation();
+    }
+}
