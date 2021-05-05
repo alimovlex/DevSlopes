dbd332c92411ae92f35e63be46948bf9cb43f201
alimovlex <alimovlex@hotmail.com>
2021-05-05 23:56:20 +0700

    The GoalCells have been configured!

---
+++ iOS 11/goalpost-app/goalpost-app/Enum/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by robot on 5/5/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType:String {
+    case longTerm = "Long Term";
+    case shortTerm = "Short Term";
+}
