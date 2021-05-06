afc562aeaee58a197f6a232a148064904fa5f04a
alimovlex <alimovlex@hotmail.com>
2021-05-07 06:58:52 +0700

    The details screen has been implemented!

---
+++ iOS 10/DreamLister/DreamLister/Controller/ItemDetailsVC.swift
@@ -0,0 +1,32 @@
+//
+//  ItemDetailsVC.swift
+//  DreamLister
+//
+//  Created by robot on 5/7/21.
+//  Copyright © 2021 robot. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItem.Style.plain, target: nil, action: nil);
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
